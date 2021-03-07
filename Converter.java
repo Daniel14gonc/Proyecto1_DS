ce7a60674a46c47435d31af57deb6eaaedc528f0
SEBASTIAN ARISTONDO <ari20880@uvg.edu.gt>
2021-03-07 23:25:04 +0000

    Definicion interface operable en evaluar, operaciones

--- Converter.java
+++ Converter.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Converter{
 
   private static Converter converterInstance;
@@ -14,6 +16,51 @@ public class Converter{
     return converterInstance;
   }
 
+  public static String ConverterPrePos(String Prefix) {
 
+    Stack<String> almacen = new Stack<String>();
+
+    try
+    {
+      for (int i = Prefix.length() - 1; i >= 0; i--) {
+        if (Operadores(Prefix.charAt(i))) {
+
+          String Signo1;
+          String Signo2;
+
+          Signo1 = almacen.peek();
+          almacen.pop();
+          Signo2 = almacen.peek();
+          almacen.pop();
+
+          String PostfixLine = Signo1 + Signo2 + Prefix.charAt(i);
+          almacen.push(PostfixLine);
+   
+        } else {
+          almacen.push(Prefix.charAt(i) + "");
+        }
+      }
+        return almacen.peek();
+    }
+    catch(Exception e)
+    {
+      return "Expresión invalida";
+    }
+  }
+
+  public static boolean Operadores(char Dato) {
+
+    if (Dato == '+' || Dato == '-' || Dato == '*' || Dato == '/' || Dato == '^') {
+      return true;
+    }
+    return false;
+  }
+
+  /*public static void main(String[] args) {
+
+    String BPrueba = "+12*3/2";
+    System.out.println("\nOperacion en Prefix: " + BPrueba);
+    System.out.println("Operacion en Postfix: " + ConverterPrePos(BPrueba) + "\n");
+  }*/
 
 }
--- Evaluar.java
+++ Evaluar.java
@@ -13,24 +13,27 @@ public class Evaluar implements Operable, Comparable{
   }
 
   public float Suma(String Dato1, String Dato2){
-
-    float Num = 0;
-    return Num;
+    float n1 = (float)Dato1;
+    float n2 = (float)Dato2;
+    return  n1+n2;
   }
 
   public float Resta(String Dato1, String Dato2){
-    float Num = 0;
-    return Num;
+    float n1 = (float)Dato1;
+    float n2 = (float)Dato2;
+    return  n1-n2;
   } 
 
   public float Multiplicacion(String Dato1, String Dato2){
-    float Num = 0;
-    return Num;
+    float n1 = (float)Dato1;
+    float n2 = (float)Dato2;
+    return  n1*n2;
   }  
 
   public float Division(String Dato1, String Dato2){
-    float Num = 0;
-    return Num;
+    float n1 = (float)Dato1;
+    float n2 = (float)Dato2;
+    return  n1/n2;
   }  
 
   public String Equal(){
